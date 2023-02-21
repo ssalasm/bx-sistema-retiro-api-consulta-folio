d9816326c45ae590cf209bebce0a170211c0d687
Sebastián Salas <100061338+ssalasm@users.noreply.github.com>
2023-02-21 12:53:35 -0300

    dockerize app

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { FoliosModule } from './folios/folios.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Folio } from './folios/entities/folio.entity';
-import * as dotenv from 'dotenv'
+import * as dotenv from 'dotenv';
 
 dotenv.config();
 
@@ -10,7 +10,12 @@ dotenv.config();
   imports: [
     TypeOrmModule.forRoot({
       type: 'postgres',
-      url: process.env.POSTGRES_URL,
+      //url: process.env.POSTGRES_URL,
+      host: process.env.POSTGRES_HOST,
+      port: parseInt(process.env.POSTGRES_PORT, 4),
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DB,
       entities: [Folio],
       synchronize: false,
       retryDelay: 3000,

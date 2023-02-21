3aa1463a4c2c52d12a8005629ae0dda95132a44d
Sebastián Salas <100061338+ssalasm@users.noreply.github.com>
2023-02-20 22:50:20 -0300

    update controllers & services

--- src/folios/controllers/folios.controller.ts
+++ src/folios/controllers/folios.controller.ts
@@ -1,11 +1,23 @@
 import { Controller, Get, Param, Post, Body, Put, Delete } from '@nestjs/common';
+import { FoliosService } from '../services/folios.service';
 
 @Controller('api/folios')
 export class FoliosController {
 
+    constructor(
+        private foliosService: FoliosService
+    ){}
+
     @Get()
     findAll() {
-        return [1, 2, 3]
+        return this.foliosService.findAll();
+    }
+
+    @Get('generar')
+    async generarNumeroPickup(): Promise<any> {
+        const folio = await this.foliosService.generarNumeroPickup();
+        const filteredData = folio.map(folio => folio.o_pickup_seq);
+        return filteredData;
     }
 
 }
--- src/folios/services/folios.service.ts
+++ src/folios/services/folios.service.ts
@@ -1,4 +1,23 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Folio } from '../entities/folio.entity';
 
 @Injectable()
-export class FoliosService {}
+export class FoliosService {
+
+    constructor(
+        @InjectRepository(Folio) private foliosRepo: Repository<Folio>,
+    ) {}
+
+    findAll() {
+        // return this.foliosRepo.find();
+        return this.foliosRepo.query('');
+    }
+
+    async generarNumeroPickup(): Promise<any> {
+        const folio = await this.foliosRepo.query('');
+        return folio;
+    }
+
+}

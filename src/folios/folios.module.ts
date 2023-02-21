32ea62102df192a6e16fb7c1ebb0069c02f1a796
Sebastián Salas <100061338+ssalasm@users.noreply.github.com>
2023-02-20 22:38:37 -0300

    update folios module

--- src/folios/folios.module.ts
+++ src/folios/folios.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Folio } from './entities/folio.entity';
 import { FoliosService } from './services/folios.service';
 import { FoliosController } from './controllers/folios.controller';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([Folio])
+  ],
   providers: [FoliosService],
   controllers: [FoliosController]
 })
